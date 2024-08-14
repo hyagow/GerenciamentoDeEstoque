777e3ab3eb24f9cfdd65ed5c3615535869ce02ab
Hyago Andrade <hyagosdj@gmail.com>
2024-08-14 00:06:31 -0300

    feat: insert ProductModelRequest(BaseModel) in models

--- models/models.py
+++ models/models.py
@@ -8,3 +8,11 @@ class ProductModelResponse(BaseModel):
   qnt_estoque = Column(Integer, index=True)
   preco = Column(Float, index=True)
   loc_deposito = Column(String, index=True)
+
+
+class ProductModelRequest(BaseModel):
+  nome = Column(String, index=True)
+  categoria = Column(String, index=True)
+  qnt_estoque = Column(Integer, index=True)
+  preco = Column(Float, index=True)
+  loc_deposito = Column(String, index=True)

aba4a639190b84f2b12dfaf179485a4009358ad2
Hyago Andrade <hyagosdj@gmail.com>
2024-08-16 22:04:44 -0300

    feat: insert descricao

---
+++ descricao.py
@@ -0,0 +1,6 @@
+descricao = "Você foi contratado por uma empresa de comércio eletrônico que está em rápido \
+crescimento.\
+A empresa enfrenta dificuldades para gerenciar seu estoque de forma eficiente, o que tem levado a \
+problemas como falta de produtos, excesso de estoque e dificuldade em rastrear a localização dos \
+itens nos depósitos. A empresa deseja desenvolver um sistema de gerenciamento de estoque que \
+otimize suas operações, melhorando a eficiência e a precisão na gestão de produtos."

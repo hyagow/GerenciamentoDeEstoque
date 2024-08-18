9627eba4ecafa40b9ca233c621c8b2e40f9c2d94
Hyago Andrade <hyagosdj@gmail.com>
2024-08-17 23:51:22 -0300

    feat: removed object list database

--- db/database.py
+++ db/database.py
@@ -7,9 +7,3 @@ SQLALCHEMY_DATABASE_URL = "mysql+mysqlclient://root:123123123@localhost/my_datab
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base: DeclarativeMeta = declarative_base()
-
-
-my_database = [
-  {'id':1, 'nome':'produto1', 'categoria': 'cat1', 'qnt_estoque': 10, 'preco':19238.12},
-  {'id':2, 'nome':'produto2', 'categoria': 'cat2', 'qnt_estoque': 20, 'preco':238.10},
-]

80f6f09a4ec7595d007c08c754fd3b3ded116aa4
Hyago Andrade <hyagosdj@gmail.com>
2024-09-06 22:50:36 -0300

    feat: inserted ProdutoBase(BaseModel) in schemas

---
+++ schemas/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class ProdutoBase(BaseModel):
+    codigo: str
+    nome: str
+    descricao: Optional[str] = None
+    categoria: Optional[str] = None
+    preco: float
+    quantidade_inicial: int
+    localizacao: Optional[str] = None
+

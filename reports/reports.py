139707f8bbba59542328f10ef9ac7077608c170a
Hyago Andrade <hyagosdj@gmail.com>
2024-09-23 14:32:14 -0300

    feat: insert relatorio_estoque_baixo in reports

--- reports/reports.py
+++ reports/reports.py
@@ -2,3 +2,5 @@ from sqlalchemy.orm import Session
 from models.models import Produto, Movimentacao
 from datetime import datetime
 
+def relatorio_estoque_baixo(db: Session, threshold: int):
+    return db.query(Produto).filter(Produto.quantidade_inicial < threshold).all()

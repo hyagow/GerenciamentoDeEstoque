82569e9648b315a41e701cfee3c62a7e59d375b1
Hyago Andrade <hyagosdj@gmail.com>
2024-09-24 15:38:55 -0300

    feat: insert relatorio_excesso_estoque in reports

--- reports/reports.py
+++ reports/reports.py
@@ -4,3 +4,6 @@ from datetime import datetime
 
 def relatorio_estoque_baixo(db: Session, threshold: int):
     return db.query(Produto).filter(Produto.quantidade_inicial < threshold).all()
+
+def relatorio_excesso_estoque(db: Session, threshold: int):
+    return db.query(Produto).filter(Produto.quantidade_inicial > threshold).all()

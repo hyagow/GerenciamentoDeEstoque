112415b254763dfbd6b14eee625a59e9e1b6d7f7
Hyago Andrade <hyagosdj@gmail.com>
2024-09-29 23:37:18 -0300

    feat: insert commentary in reports

--- reports/reports.py
+++ reports/reports.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from models.models import Produto, Movimentacao
 from datetime import datetime
 
+
+# Relatórios da API
 def relatorio_estoque_baixo(db: Session, threshold: int):
     return db.query(Produto).filter(Produto.quantidade_inicial < threshold).all()
 
